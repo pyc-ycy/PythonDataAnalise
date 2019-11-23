193d986bafc5b5f7730cd9ababe088b42d79d6dd
御承扬 <2923616405@qq.com>
2019-11-23 21:53:12 +0800

    Initial commit

---
+++ Set01/test.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+# Time : 2019/11/23 下午 9:37 
+# Author : 御承扬
+# e-mail:2923616405@qq.com
+# project:  Python大数据
+# File : test.py 
+# @software: PyCharm
+
+
+if __name__ == "__main__":
+    print("Hello world")
