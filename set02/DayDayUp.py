4c0eb2325603a54db0c8d15b0bf042f1a5d6206e
ycy-ycy <2923616405@qq.com>
2020-03-16 21:51:01 +0800

    Update

--- set02/DayDayUp.py
+++ set02/DayDayUp.py
@@ -5,3 +5,18 @@
 # project: Python大数据
 # File: DayDayUp
 # PyCharm
+
+
+def func1():
+    day_factor = 0.005
+    up = pow(1 + day_factor, 365)
+    down = pow(1 - day_factor, 365)
+    print("向上：{:.2f}，向下：{:.2f}".format(up, down))
+
+
+if __name__ == "__main__":
+    dayUp = pow(1.001, 365)
+    dayDown = pow(0.999, 365)
+    print("向上：{:.2f}，向下：{:.2f}".format(dayUp, dayDown))
+    print("权重为：0.005时：")
+    func1()
