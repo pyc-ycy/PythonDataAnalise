eb5750d01d2ffeb23da24f5b985c8a9fec53afd1
7wc98#14 <2923616405@qq.com>
2020-04-16 21:50:32 +0800

    Update

---
+++ aboutTime/code01.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+# Time:2020/4/16 21:29
+# Author: 御承扬
+# e-mail:2923616405@qq.com
+# project: Python大数据
+# File: code01
+# PyCharm
+
+# Use time library
+import time
+
+
+if __name__ == "__main__":
+    print(time.time())
+    print(time.gmtime())
+    t = time.gmtime()
+    print(time.strftime("%Y-%m-%d %H:%M:%S", t))
---
+++ aboutTime/code02.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+# Time:2020/4/16 21:30
+# Author: 御承扬
+# e-mail:2923616405@qq.com
+# project: Python大数据
+# File: code02
+# PyCharm
